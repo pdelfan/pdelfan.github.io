2882c38f085f769d51f590b302cfae071470f0cb
pouria <pdelfan@gmail.com>
2020-12-21 15:04:17 -0800

    webp icon

--- my-site/src/components/header.js
+++ my-site/src/components/header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import { graphql } from "gatsby"
 
 const Header = () => {
   return (
@@ -9,7 +8,7 @@ const Header = () => {
         <div className="nav-left">
           <AniLink cover to="../../../" bg="#B1EBB7" direction="down">
             {" "}
-            <img src="../icon.png" alt="site icon" width="35" height="35"/>
+            <img src="../icon.webp" alt="site icon" width="35" height="35"/>
           </AniLink>
         </div>
         <div className="nav-right">
